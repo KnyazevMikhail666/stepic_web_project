79c8dc6952ab50803f911101ee2c549b73a4382c
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-05-04 19:37:24 +0300

    /

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
-
-
 def app(environ, start_response):
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = b''.join([bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')])
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
