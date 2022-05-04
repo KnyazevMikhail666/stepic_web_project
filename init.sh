d88d9f844ace8a4c9420d0a752f7909398ddc188
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-05-04 19:10:10 +0300

    /

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b "0.0.0.0:8080" hello:app
+
+gunicorn -b "0.0.0.0:8080"  hello:app
