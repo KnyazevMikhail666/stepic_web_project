857ec1a888afe1c23103d9eb1765b0a0a41fe41b
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-05-04 18:03:31 +0300

    /

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b '127.0.0.1:8080' hello:app
+sudo gunicorn -b "0.0.0.0:8080" hello:app
