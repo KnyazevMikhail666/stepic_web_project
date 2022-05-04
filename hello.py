13e55a5e75f38d733dfb3d99c37dbaf3af6d8ed7
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-05-04 19:17:00 +0300

    /

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ def app(environ, start_response):
         ('Content-Length', str(len(body)))
     ]
     start_response(status, response_headers)
-    return iter([body.encode('utf-8')])
+    return iter([body])
