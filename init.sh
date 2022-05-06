a83872a44d9b877910e0e60458b34c8ba731d6f4
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-05-06 18:01:54 +0300

    /

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ sudo /etc/init.d/gunicorn restart
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 
-gunicorn  --bind="0.0.0.0:8080"   hello:test
+gunicorn  --bind="0.0.0.0:8080"   hello:app
