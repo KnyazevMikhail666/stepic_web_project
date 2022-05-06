83825670fc6b6286a3fbe67c46f1032ed92cd205
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-05-06 17:55:15 +0300

    /

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
-sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo /etc/init.d/nginx restart
 
 gunicorn  --bind="0.0.0.0:8080" -c ~/web/hello.py  hello:test
